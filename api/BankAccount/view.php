8ef1f60d85baf7821f9d5b0802d8064016f336ae
duan-le <56768212+duan-le@users.noreply.github.com>
2020-12-05 12:09:05 -0700

    Remove comments

--- api/BankAccount/view.php
+++ api/BankAccount/view.php
@@ -6,42 +6,27 @@
   include_once '../../config/Database.php';
   include_once '../../models/BankAccount.php';
 
-  // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
-
-  // Instantiate BankAccount object
-  $bank_account = new BankAccount($db);
-
-  // BankAccount read query
+	$bank_account = new BankAccount($db);
   $result = $bank_account->view();
-  
-  // Get row count
   $num = $result->rowCount();
 
-  // Check if any BankAccount
   if ($num > 0) {
-		// Cat array
 		$bank_account_arr = array();
 		$bank_account_arr['data'] = array();
 
 		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
 			extract($row);
-
 			$bank_account_item = array(
 				'account_number' => $account_number,
 				'account_type' => $account_type,
 				'employee_id' => $employee_id
 			);
-			// Push to "data"
 			array_push($bank_account_arr['data'], $bank_account_item);
 		}
-
-		// Turn to JSON & output
 		echo json_encode($bank_account_arr);
-
   } else {
-		// No Bank Account
 		echo json_encode(
 			array('message' => 'No Bank Account Found')
 		);
