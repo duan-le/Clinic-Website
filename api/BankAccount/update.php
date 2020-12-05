3ddee669462f0e240f5616d43a6d24856e6b98bb
duan-le <56768212+duan-le@users.noreply.github.com>
2020-12-05 12:50:04 -0700

    Add update

---
+++ api/BankAccount/update.php
@@ -0,0 +1,27 @@
+<?php 
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/BankAccount.php';
+
+  $database = new Database();
+  $db = $database->connect();
+	$bank_account = new BankAccount($db);
+  $data = json_decode(file_get_contents("php://input"));
+  
+  $bank_account->account_number = $data->account_number;
+  $bank_account->account_type = $data->account_type;
+  $bank_account->employee_id = $data->employee_id;
+
+  if ($bank_account->insert()) {
+    echo json_encode(
+      array('message' => 'Bank Account Created')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'Bank Account Not Created')
+    );
+  }
--- model/BankAccount.php
+++ model/BankAccount.php
@@ -42,4 +42,21 @@
 				return false;
 			}
 		}
+
+		public function update() {
+			$query = 'UPDATE ' . $this->table . ' SET account_number = ?, account_type = ? WHERE employee_id = ?';
+			$stmt = $this->conn->prepare($query);
+			$this->account_number = htmlspecialchars(strip_tags($this->account_number));
+			$this->account_type = htmlspecialchars(strip_tags($this->account_type));
+			$this->employee_id = htmlspecialchars(strip_tags($this->employee_id));
+			$stmt->bindParam(1, $this->account_number);
+			$stmt->bindParam(2, $this->account_type);
+			$stmt->bindParam(3, $this->employee_id);
+			if ($stmt->execute()) {
+				return true;
+			} else {
+				printf("Error: %s.\n", $stmt->error);
+				return false;
+			}
+		}
 	}
